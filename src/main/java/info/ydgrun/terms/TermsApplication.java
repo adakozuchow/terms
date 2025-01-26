9cb9f509fbc4e508d1c5a647be1d91e5bd58b2fd
418 Teapot <adam.kozuchowski@tlen.pl>
2025-01-26 12:31:16 +0100

    Init commit

---
+++ src/main/java/info/ydgrun/terms/TermsApplication.java
@@ -0,0 +1,13 @@
+package info.ydgrun.terms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/ydgrun/terms/TermsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ydgrun.terms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
